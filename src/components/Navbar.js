daef9cb55abe448003130f3e478ab2f04c863c74
AdeWang0629 <adswang.dev@gmail.com>
2024-03-20 02:04:50 +0900

    complete PC ui - 2

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { navItmes } from "../data";
 
 const Navbar = ({selectedItem, setSelectedItem, setSelectedBread}) => {
@@ -21,14 +22,16 @@ const Navbar = ({selectedItem, setSelectedItem, setSelectedBread}) => {
 
                 {
                     navItmes.map((item, index) => (
-                        <p className={`flex text-xl text-slate-400 py-3 items-center cursor-pointer ${selectedItem === index && ('justify-between border-r-4 border-blue-500 pr-1')}`} onClick={() => handleSelect(index)} key={index}>
-                            {item}
-                            { selectedItem === index && (
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-                                    <path fillRule="evenodd" clipRule="evenodd" d="M6.50558 8.50564C6.20184 8.80938 6.20184 9.30184 6.50558 9.60558L11.95 15.05C12.2538 15.3538 12.7462 15.3538 13.05 15.05L18.4944 9.60558C18.7982 9.30184 18.7982 8.80938 18.4944 8.50564C18.1907 8.2019 17.6982 8.2019 17.3945 8.50564L12.5 13.4001L7.60552 8.50564C7.30178 8.2019 6.80932 8.2019 6.50558 8.50564Z" fill="#888888"/>
-                                </svg>
-                            )}
-                        </p>
+                            <Link to='/'>
+                                <p className={`flex text-xl text-slate-400 py-3 items-center cursor-pointer ${selectedItem === index && ('justify-between border-r-4 border-blue-500 pr-1')}`} onClick={() => handleSelect(index)} key={index}>
+                                    {item}
+                                    { selectedItem === index && (
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                            <path fillRule="evenodd" clipRule="evenodd" d="M6.50558 8.50564C6.20184 8.80938 6.20184 9.30184 6.50558 9.60558L11.95 15.05C12.2538 15.3538 12.7462 15.3538 13.05 15.05L18.4944 9.60558C18.7982 9.30184 18.7982 8.80938 18.4944 8.50564C18.1907 8.2019 17.6982 8.2019 17.3945 8.50564L12.5 13.4001L7.60552 8.50564C7.30178 8.2019 6.80932 8.2019 6.50558 8.50564Z" fill="#888888"/>
+                                        </svg>
+                                    )}
+                                </p>
+                            </Link>
                         )
                     )
                 }
